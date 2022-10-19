14c065ae1b17640b66eb3b2920a84f6b6d808790
xstartstaticx <daniel.reiller@icloud.com>
2022-10-19 15:02:05 +0200

    added solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -10,12 +11,14 @@ const App = () => {
       <nav>
         <Link to="home">Home</Link>
         <Link to="about">About</Link>
+        <Link to="contact">Contact</Link>
       </nav>
 
       <main>
         <Routes>
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </main>
     </>
